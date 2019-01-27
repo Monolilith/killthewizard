0870d63d7aac8fc4f649bcaeee4d521ef7cc6897
unknown <s179069@students.aie.edu.au>
2019-01-27 11:42:33 -0800

    Add main menu & scene changing

---
+++ Assets/XanderStuff/Scripts/MainMenuUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+    [SerializeField]
+    private string gameSceneName;
+    [SerializeField]
+    private GameObject startPanel;
+    [SerializeField]
+    private GameObject gamesPanel;
+    [SerializeField]
+    private TMPro.TMP_InputField nameField;
+
+
+
+    public void GoToGamesPanel()
+    {
+        startPanel.SetActive(false);
+        gamesPanel.SetActive(true);
+    }
+
+    public void TryStartGame()
+    {
+        if(nameField.text != "")
+        {
+            SceneBridge.Instance.characterName = nameField.text;
+            SceneManager.LoadSceneAsync(gameSceneName);
+        }
+    }
+
+}
---
+++ Assets/XanderStuff/Scripts/SceneBridge.cs
@@ -0,0 +1,20 @@
+﻿
+public class SceneBridge {
+
+    private static SceneBridge instance;
+	public static SceneBridge Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new SceneBridge();
+
+            return instance;
+        }
+    }
+
+
+
+    public string characterName;
+
+}
